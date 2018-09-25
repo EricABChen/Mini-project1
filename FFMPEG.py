55dfcffde2ae6e6cb4a84f5f42f0f40ab59d831e
EricABChen <43049334+EricABChen@users.noreply.github.com>
2018-09-25 11:36:01 -0400

    Add files via upload

---
+++ FFMPEG.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 24 14:30:17 2018
+
+@author: 18004
+"""
+
+import os
+import subprocess
+# use ffmpeg to convert images into video
+# detect images with path:/users/18004/Desktop/ec601/project1/
+# convert the image into proper size, and convert them into a mp4 vedio named 'Myvedio.mp4'
+subprocess.call('ffmpeg -f image2 -framerate 1 -s 1920*1080 -i r/users/18004/Desktop/ec601/project1/ -crf 25 -pix_fmt yuv420p Myvideo.mp4')
