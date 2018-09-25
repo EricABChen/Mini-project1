6dbf7ed13406362dcf0d344618407ec4fb6e89a0
EricABChen <43049334+EricABChen@users.noreply.github.com>
2018-09-25 13:36:27 -0400

    Add files via upload

---
+++ get_twitter.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# get twitter API form tweepy
+import tweepy 
+import wget
+
+
+# use your own token to authorize the function of the API
+consumer_key = "your API key"
+consumer_secret = "your API secret key"
+access_key = "your access token"
+access_secret = "your access token secret"
+
+
+# authorize twitter, initialize tweepy
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
+
+
+#this  funtion download tweets from tweet
+def get_tweet(image_name):
+    
+    image = []    
+    new_tweets = api.user_timeline(screen_name='football',
+                                   count=10, include_rts=False,
+                                   exclude_replies=True)
+    
+    image.extend(new_tweets)
+    
+    error_count = 0
+    image_files = []
+    for item in new_tweets:
+        image = item.entities.get('media', [])
+        image_files.append(image[item]['media_url'])
+        if image_files.endswith(".jpg"):
+            wget.download(image_files)
+        else:
+            print("Sorry, but there is no image here!")
+            error_count += 1
+    
+    print("There exists: " , error_count, "errors!")
+    
+
+
+    
+   
