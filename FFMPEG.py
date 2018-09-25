e9dd5e01657ea8bae481c36a7e6aecf561766372
EricABChen <43049334+EricABChen@users.noreply.github.com>
2018-09-25 12:46:46 -0400

    Update FFMPEG.py

--- FFMPEG.py
+++ FFMPEG.py
@@ -4,4 +4,4 @@ import subprocess
 # use ffmpeg to convert images into video
 # detect images with path:/users/18004/Desktop/ec601/project1/
 # convert the image into proper size, and convert them into a mp4 vedio named 'Myvedio.mp4'
-subprocess.call('ffmpeg -f image2 -framerate 1 -s 1920*1080 -i r/users/18004/Desktop/ec601/project1/ -crf 25 -pix_fmt yuv420p Myvideo.mp4')
+subprocess.call('ffmpeg', '-f', 'image2', '-framerate', '1', '-s', '1920*1080', '-i', 'r/users/18004/Desktop/ec601/project1/', '-crf', '25', '-pix_fmt', 'yuv420p', 'Myvideo.mp4')
